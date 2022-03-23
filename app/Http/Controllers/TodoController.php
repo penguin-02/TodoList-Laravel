d235738f849456473ccb53e77f36093039dd74fd
penguin <grizzly@Macintosh.local>
2022-03-23 11:12:21 +0900

    first commit

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Todo;
+
+class TodoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+      // 期限が近いものから順に表示する、期限がないものは最後に持っていく
+      $todos = Todo::orderByRaw('`deadline` IS NULL ASC')->orderBy('deadline')->get();
+
+      return view('todos.index', [
+          'todos' => $todos,
+      ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+      $request->validate([
+            'newTodo'     => 'required|max:100',
+            'newDeadline' => 'nullable|after:"now"',
+        ]);
+
+        // DBに保存
+        Todo::create([
+            'todo'     => $request->newTodo,
+            'deadline' => $request->newDeadline,
+        ]);
+
+        return redirect()->route('todos.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+      $todo = Todo::find($id);
+
+      return view('todos.edit', [
+          'todo' => $todo,
+      ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+      $request->validate([
+          'updateTodo'     => 'required|max:100',
+          'updateDeadline' => 'nullable|after:"now"',
+      ]);
+
+      $todo = Todo::find($id);
+
+      $todo->todo     = $request->updateTodo;
+      $todo->deadline = $request->updateDeadline;
+
+      $todo->save();
+
+      return redirect()->route('todos.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+      $todo = Todo::find($id);
+
+      $todo->delete();
+
+      return redirect()->route('todos.index');
+    }
+}
---
+++ resources/views/todos/index.blade.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Todoリスト</title>
+    <link href="https://use.fontawesome.com/releases/v5.15.1/css/all.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container mt-3 box1">
+        <img src="/image/logo.svg" alt="logo" height="70" width="200" class="logo">
+        <div class="btn1">
+          <button type="button" class="btn btn-dark">Dark</button>
+
+        </div>
+    </div>
+    <div class="container mt-3">
+        <div class="container mb-4">
+            {!! Form::open(['route' => 'todos.store', 'method' => 'POST']) !!}
+            {{ csrf_field() }}
+                <div class="row">
+                    {{ Form::text('newTodo', null, ['class' => 'form-control col-8 mr-5']) }}
+                    {{ Form::date('newDeadline', null, ['class' => 'mr-5']) }}
+                    <!--{{ Form::submit('新規追加', ['class' => 'btn btn-primary']) }}-->
+                    <button type="submit" class="btn-add ">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" fill="currentColor" class="bi bi-plus-circle-fill" viewBox="0 0 16 16">
+                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
+                      </svg>
+                    </button>
+                </div>
+            {!! Form::close() !!}
+        </div>
+        {{-- エラー表示 ここから --}}
+        @if ($errors->has('newTodo'))
+            <p class="alert alert-danger">{{ $errors->first('newTodo') }}</p>
+        @endif
+        @if ($errors->has('newDeadline'))
+            <p class="alert alert-danger">{{ $errors->first('newDeadline') }}</p>
+        @endif
+        {{-- エラー表示 ここまで --}}
+
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col" style="width: 60%">Todo</th>
+                    <th scope="col">期限</th>
+                    <th scope="col"></th>
+                    <th scope="col"></th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($todos as $todo)
+                    <tr>
+                      <th scope="row" class="todo">{{ $todo->todo }}</th>
+                      <td>{{ $todo->deadline }}</td>
+                      <td>
+                        <a href="{{ route('todos.edit', $todo->id) }}" class="edit-icon">
+                          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#4169e1" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                            <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
+                            <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
+                          </svg>
+                        </a>
+                      </td>
+                      {!! Form::open(['route' => ['todos.destroy', $todo->id], 'method' => 'POST']) !!}
+                      {{ csrf_field() }}
+                      {{ method_field('DELETE') }}
+                        <!--<td>{{ Form::submit('削除', ['class' => 'btn btn-danger']) }}</td>-->
+                        <td>
+                        <button type="submit" class="delete-icon ">
+                          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="#ea553a" class="bi bi-trash3-fill" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M6 1.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5v1H6v-1Zm5 0v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5ZM4.5 5.029a.5.5 0 1 1 .998-.06l.5 8.5a.5.5 0 0 1-.998.06l-.5-8.5Zm6.53-.528a.5.5 0 0 1 .47.528l-.5 8.5a.5.5 0 1 1-.998-.058l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
+                          </svg>
+                        </button>
+                        </td>
+                      {!! Form::close() !!}
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+    <footer class="footer">
+	     <div class="container text-center">
+	        <p class="text-muted">©︎Syusuke Hayashi</p>
+	      </div>
+    </footer>
+</body>
+</html>
